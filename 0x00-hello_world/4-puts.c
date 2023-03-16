cc323a37b144f6df0c5c1b5632618959297e2d41
phemmymoxie <gphemmy126@gmail.com>
2023-03-16 05:41:42 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *main main entry of the code
+ *
+ *Return: always 0
+ */
+int main(viod)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
