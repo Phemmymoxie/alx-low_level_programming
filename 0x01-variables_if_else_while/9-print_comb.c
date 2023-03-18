f99242a900354263575229c103a5303e9777e78f
phemmymoxie <gphemmy126@gmail.com>
2023-03-17 21:57:15 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints all possible different combination of numbers
+ *
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+	char f;
+
+	for (f = '0'; f <= '9'; f++)
+	{
+		putchar(f);
+		if (f != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
