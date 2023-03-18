36d28140a7f6d469a8eeb97d5c929ad779ee7638
phemmymoxie <gphemmy126@gmail.com>
2023-03-18 06:11:40 -0700

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - A function that print combination of numbers from 0 -9
+ * in ascending order without reapeted combination.
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int c;
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			if (c != b && c >= b)
+			{
+			putchar(b);
+			putchar(c);
+			if (c != EOF && b != EOF)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
