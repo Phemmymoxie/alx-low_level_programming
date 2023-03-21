f8d782167b53e966dde01da90709c2cbc61c5656
phemmymoxie <gphemmy126@gmail.com>
2023-03-21 13:06:51 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * jack_bauer - function that prints time starting from 00:00 to 23:59.
  *
  * Return: Always 0.
  */
@@ -10,6 +10,7 @@ void jack_bauer(void)
 {
 	int j;
 	int k;
+
 	for (j = 0; j <= 23; j++)
 	{
 		for (k = 0; k <= 59; k++)
