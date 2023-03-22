bfdf606ed64c717b4ee439832180e2ef106212f2
phemmymoxie <gphemmy126@gmail.com>
2023-03-21 20:19:04 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - code that add two integers and return the result.
+ * @q: the first argument
+ * @r: the second argument
+ * Return: Always 0.
+ */
+
+int add(int q, int r)
+{
+	int addup = q + r;
+
+	return (addup);
+}
