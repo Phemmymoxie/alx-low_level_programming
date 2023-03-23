a70f946b0c2ac8cbfc38c47da93fabd3d037ee82
phemmymoxie <gphemmy126@gmail.com>
2023-03-22 19:24:49 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 /**
+ * main - main function to be executed
  *
- *
- *
+ * Return: Always (0)
  */
 
-int main()
+int main(void)
 {
 	int a = 0;
 	long int f = 1;
-	long int l = 2;
+	long int l = f;
 	long int nxt = l + f;
+
 	while (nxt < 4000000)
 	{
-	l = f;
-	f = nxt;
-	nxt = l + f;
 	if (nxt % 2 == 0)
 	{
 	a += nxt;
 	}
+	l = f;
+	f = nxt;
+	nxt = l + f;
 	}
 	printf("%d\n", a);
 	return (0);
