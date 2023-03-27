e4ed6707dc1d2423dc7b67720d5deb06c5b50728
phemmymoxie <gphemmy126@gmail.com>
2023-03-27 13:02:36 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - check the code
+ * @s: string argument passed
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int n;
+	char ch;
+	char str[100];
+
+	n = strlen(s);
+	strcpy(str, s);
+	for (i = 0; i < n / 2; i++)
+	{
+		ch = str[i];
+		str[i] = str[n - i - 1];
+		str[n - i - 1] = ch;
+	}
+	printf("%s\n", str);
+}
