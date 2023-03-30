75cee41227c0caf8fd7f80165760d3c4b750fad6
phemmymoxie <gphemmy126@gmail.com>
2023-03-30 11:18:39 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+char *cap_string(char *s)
+{
+	int g = 0, a;
+	int sep_value[] = {32, 9, 10, 44, 59, 46, 33, 34, 40, 41, 12, 123, 125};
+
+	if (s[g] >= 97 && s[g] <= 122)
+	{
+		s[g] -= 32;
+		g++;
+	}
+	while (s[g] != '\0')
+	{
+		for (a = 0; a < 13; a++)
+		{
+			if (s[g] == sep_value[a])
+			{
+				if (s[g + 1] >= 97 && s[g + 1] <= 122)
+				{
+					s[g + 1] -= 32;
+					
+					break;
+				}
+			
+		a++;	}
+		}
+	g++;
+
+	}
+	return (s);
+}
