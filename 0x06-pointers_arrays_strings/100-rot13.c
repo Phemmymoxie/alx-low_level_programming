ca9a45905ae481dc7c0045119a21b30ebd7460cb
phemmymoxie <gphemmy126@gmail.com>
2023-03-30 16:26:21 -0700

    more pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * rot13 - encodes a string
+ * @ch: input variables
+ * Return: always return dest
+ */
+
 char *rot13(char *ch)
 {
 	char *nig = ch;
 	int i;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (*ch)
 	{
-		for (i = 0; i < 13; i++)
+		for (i = 0; a[i] != '\0'; i++)
 		{
-			if ((*ch >= 65 && *ch <= 77) || (*ch >= 97 &&  *ch <= 109))
+			if (*ch == a[i])
 			{
-				*ch += 13;
-			}
-
-			else if ((*ch >= 78 && *ch <= 90) || (*ch >= 110 && *ch <= 122))
-			{
-				*ch -= 13;
+				*ch = b[i];
+				break;
 			}
 		}
 	ch++;
