986d91be4b57dcbe9591c8127d4165c99dbc7e02
phemmymoxie <gphemmy126@gmail.com>
2023-04-03 12:50:42 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "main.h"
+/**
+ * _strchr - A function that checks a string and return a pointer.
+ * where there is an occurrence of a character
+ * @s: The pointer to a string to be checked.
+ * @c: The character to be compared with the character of the string.
+ * Return: return a pointer or return null
+ * if there is no occurrence of the string.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int gem = 0;
+
+	while (s[gem] != '\0')
+	{
+		if (s[gem] == c && s[gem + 1] != '\0')
+		{
+			return (s + gem);
+		}
+		gem++;
+	}
+	return (NULL);
+}
