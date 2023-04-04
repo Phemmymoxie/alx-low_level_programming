f862ada71397fb75027ee715413086791db8a3b7
phemmymoxie <gphemmy126@gmail.com>
2023-04-03 21:45:19 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - A function that sets a string to another
+ * @s: the pointer to a string to be set
+ * @to: the pointer argument to be set to
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+
+	while (*s)
+	{
+		*s = to;
+	}
+}
