039d46f6282f609368f5e3bf54e5060da3312def
phemmymoxie <gphemmy126@gmail.com>
2023-04-05 11:40:37 -0700

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that calculates power of a number.
+ * @x: the base number.
+ * @y: the power
+ *
+ * Return: the result.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+return (x * _pow_recursion(x, y - 1));
+}
