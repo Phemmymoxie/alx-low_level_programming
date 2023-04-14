cf58617490406d97d2ee1414e0d5aff2b3e1236d
phemmymoxie <gphemmy126@gmail.com>
2023-04-14 08:54:12 -0700

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,19 +2,26 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * _realloc - a function that reallocate memory
+ * @ptr: pointer to the memory
+ * @old_size: the old size of the memory
+ * @new_size: the new size of the memory allocated
+ * Return: the pointer ptr.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	
+
 	if (new_size == old_size)
 		return (ptr);
-	
-	if (ptr == NULL)	
+
+	if (ptr == NULL)
 		ptr = malloc(new_size);
-		
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (ptr);
+		return (NULL);
 	}
 	free(ptr);
 	ptr = malloc(new_size);
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+	long int min, max;
+
+	if (argc > 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	min = atoi(argv[1]);
+	max = atoi(argv[2]);
+
+	if (min > 0 && max > 0)
+	{
+		printf("%lu\n", min * max);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+return (0);
+}
+
