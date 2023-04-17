0b0e96620b79d151bccf01c9bf4ddb3576264ffe
phemmymoxie <gphemmy126@gmail.com>
2023-04-17 12:55:11 -0700

    structure, typdef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Discription: A (dog) struct that creates
  * an attribute name the dog, age and owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
