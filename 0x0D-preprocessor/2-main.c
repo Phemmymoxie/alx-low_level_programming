0dbe464a9a3174382c4331e04b47466fa4b6bf43
phemmymoxie <gphemmy126@gmail.com>
2023-04-17 12:05:48 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints a file
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
