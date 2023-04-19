a455b8b560b8d771e8932b3cbc9ca824bb1f1954
phemmymoxie <gphemmy126@gmail.com>
2023-04-19 13:21:05 -0700

    function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
 
@@ -43,8 +44,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
-		return (-1);
-
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -57,8 +60,10 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
-		return (-1);
-
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
 
