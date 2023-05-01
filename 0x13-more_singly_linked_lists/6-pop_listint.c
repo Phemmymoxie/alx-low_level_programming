febb18856410830942f80ba9f8b1e36133f27f4a
phemmymoxie <gphemmy126@gmail.com>
2023-04-30 19:23:32 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - function that deletes the head node of a list
+ * pop_listint - function that deletes the hea node of a list
  * @head: the double pointer to the head node
  *
  * Return: the value of the next node
@@ -9,16 +9,17 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *res;
+	unsigned int ret;
 
 	if (*head == NULL)
 	{
 		return (0);
 	}
-
+	ret = (*head)->n;
 	res = *head;
 	*head = res->next;
 	free(res);
 	res = NULL;
 
-return ((*head)->n);
+return (ret);
 }
