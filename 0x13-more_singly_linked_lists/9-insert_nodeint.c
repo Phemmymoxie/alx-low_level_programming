7740847b542ce4a37e33c1b0accdc83c243ec753
phemmymoxie <gphemmy126@gmail.com>
2023-05-03 19:40:18 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (NULL);
+		return (new_node);
 	}
 
 	idx--;
