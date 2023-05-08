73fb029532f1ab692578757158b8875e41a625e6
phemmymoxie <gphemmy126@gmail.com>
2023-05-08 14:33:15 -0700

    file IO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,9 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	while (*text_content != '\0')
 	{
 		wrt = write(file, text_content, 1);
