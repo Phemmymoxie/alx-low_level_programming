7ca8a37733c7b2d4eb874608c96bfa5cd4c6237b
phemmymoxie <gphemmy126@gmail.com>
2023-05-08 13:54:04 -0700

    file IO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	int file, cl, wrt, res = 0;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
