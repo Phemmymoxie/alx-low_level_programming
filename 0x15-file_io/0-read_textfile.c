72da6ee0868cce27f89ad5cfac7df24d058e451b
phemmymoxie <gphemmy126@gmail.com>
2023-05-08 10:14:29 -0700

    file IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	ret = write(1, buff, file_r);
 
-	if (ret == -1 || ret != file_r)
+	if (ret == -1 || ret < file_r)
 		return (0);
 
 free(buff);
